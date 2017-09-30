acad8663883bca995b419a00dfc788c43d1d0043
Dan Mo <dmo@princeton.edu>
2017-09-29 20:19:27 -0400

    reorganizes db migrations separates schema and migration script into its own files

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from config import *
 
 app = Flask(__name__)
+connection_string = "mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"\
+            .format(db_user=mysql_user, db_password=mysql_password, db_host=mysql_host, db_name=mysql_database)
+app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 api = Api(app)
 
--- manage.py
+++ manage.py
@@ -1,19 +1,22 @@
 from flask import Flask
+from config import *
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from config import *
+
 
 app = Flask(__name__)
 connection_string = "mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"\
             .format(db_user=mysql_user, db_password=mysql_password, db_host=mysql_host, db_name=mysql_database)
 app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+from model import db
 
-db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
 
 if __name__ == '__main__':
     manager.run()
