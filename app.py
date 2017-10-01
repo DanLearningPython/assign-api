407844b82d54e8a995f10a5b0acaa21a790104ee
Dan Mo <danmo1337@gmail.com>
2017-09-30 22:15:39 -0400

    handles 404 error

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 from config import *
+from model import *
 
 app = Flask(__name__)
 connection_string = "mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"\
@@ -11,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-api = Api(app)
+api = Api(app , catch_all_404s=True)
 
 
 def require_api_token(f):
@@ -27,9 +28,11 @@ class Home(Resource):
 
     @require_api_token
     def get(self):
+        courses = Course.query.all()
+        for course in courses:
+            print(course.course_name)
         app_name = {'assign': 'api'}
         return app_name
-
 
 
 api.add_resource(Home, '/')
--- test/app-test.py
+++ test/app-test.py
@@ -1,11 +1,17 @@
-from app import app
-import json
+from app import connection_string, app
 import unittest
+import json
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from helpers import courses_to_json
+
+from model import *
 
 
-class HomeTestCase(unittest.TestCase):
+class GeneralTestCases(unittest.TestCase):
 
     def test_index(self):
+
         tester = app.test_client(self)
         response = tester.get('/', content_type='application/json')
         data = json.loads(response.get_data(as_text=True))
@@ -13,6 +19,16 @@ class HomeTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
         self.assertEqual(data, {'assign': 'api'})
+
+    def test_404(self):
+
+        tester = app.test_client(self)
+        response = tester.get('/thisbetternotexistorsomethingisreallyreallywrong', content_type='application/json')
+        data = json.loads(response.get_data(as_text=True))
+
+        self.assertEqual(response.status_code, 404)
+
+
 
 
 if __name__ == '__main__':
