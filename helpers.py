0f790648f9bf09c3eaa3fc55534fe0119e5010c9
Dan Mo <danmo1337@gmail.com>
2017-09-30 22:12:40 -0400

    adds helper file and course to json function

---
+++ helpers.py
@@ -0,0 +1,16 @@
+import json
+
+
+def courses_to_json(courses):
+
+    json_result = []
+
+    for course in courses:
+        tmp = {
+            'id' : course.id,
+            'course_name': course.course_name,
+            'semester': course.semester_id,
+        }
+        json_result.append(tmp.copy())
+
+    return json.dumps(json_result)
