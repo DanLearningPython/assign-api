693f11c19e90e0564de396d4bdc72d3a2ede02d0
Dan Mo <danmo1337@gmail.com>
2017-09-30 22:18:57 -0400

    adds test for course gets

--- test/app-test.py
+++ test/app-test.py
@@ -29,6 +29,47 @@ class GeneralTestCases(unittest.TestCase):
         self.assertEqual(response.status_code, 404)
 
 
+class CourseTestCase(unittest.TestCase):
+
+    engine = create_engine(connection_string)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    client = app.test_client()
+
+    def setUp(self):
+
+        semester = Semester()
+        semester.season = 'Fall'
+        semester.year = 2017
+        self.session.add(semester)
+        self.session.commit()
+        self.semester = semester
+
+        course = Course()
+        course.course_name = "Test Course"
+        course.semester_id = semester.id
+        self.session.add(course)
+        self.session.commit()
+        self.course = course
+
+    def test_index(self):
+
+        courses = self.session.query(Course).all()
+        courses_json = courses_to_json(courses)
+
+        response = self.client.get('/courses', content_type='application/json')
+        data = json.loads(response.get_data(as_text=True))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(data, courses_json)
+
+    def tearDown(self):
+
+        self.session.delete(self.course)
+        self.session.commit()
+        self.session.delete(self.semester)
+        self.session.commit()
 
 
 if __name__ == '__main__':
